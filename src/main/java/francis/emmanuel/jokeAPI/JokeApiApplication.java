d3ca4d9ff2a47ea9fa1384d3b67fdb17f10c17a5
mrfrancis <efficialbusiness@mail.com>
2022-04-06 10:13:07 -0400

    added git

---
+++ src/main/java/francis/emmanuel/jokeAPI/JokeApiApplication.java
@@ -0,0 +1,13 @@
+package francis.emmanuel.jokeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/francis/emmanuel/jokeAPI/JokeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package francis.emmanuel.jokeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
